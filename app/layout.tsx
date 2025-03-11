7a56c06811eae3926da5525ec61af3999823e16b
whatDeepak <deekay9357@gmail.com>
2025-03-11 19:11:11 +0530

    Added Favicon and metadata for SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type React from "react"
 import type { Metadata } from "next"
-// import { Inter } from "next/font/google"
 import localFont from "next/font/local";
 import "./globals.css"
 
@@ -11,10 +10,40 @@ const berserker = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "HackMol 6.0 - Hack the Realms",
-  description: "Join HackMol 6.0 and hack the realms",
-    generator: 'v0.dev'
-}
+  title: "HackMol 6.0 - Punjab's Biggest Hackathon",
+  description:
+    "HackMol 6.0 is Punjab's premier 36-hour hackathon, bringing together top developers, innovators, and tech enthusiasts to solve real-world challenges in AI, Web3, IoT, and more. Compete for ₹2L+ in prizes, network with industry experts, and showcase your skills in a high-energy, hybrid-format competition.",
+  keywords: [
+    "HackMol 6.0",
+    "Hackathon",
+    "AI",
+    "Web3",
+    "IoT",
+    "Coding Competition",
+    "Developers",
+    "Punjab Tech Event",
+    "NIT Jalandhar",
+    "Software Innovation",
+  ],
+  authors: [{ name: "DSC NIT Jalandhar", url: "https://hackmol.com" }],
+  openGraph: {
+    title: "HackMol 6.0 - Punjab's Biggest Hackathon",
+    description:
+      "HackMol 6.0 is Punjab's premier 36-hour hackathon, bringing together top developers, innovators, and tech enthusiasts to solve real-world challenges in AI, Web3, IoT, and more. Compete for ₹2L+ in prizes, network with industry experts, and showcase your skills in a high-energy, hybrid-format competition.",
+    url: "https://hackmol.com",
+    siteName: "HackMol 6.0",
+    images: [{ url: "https://hackmol.com/banner.png", width: 1200, height: 630 }],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "HackMol 6.0 - Punjab's Biggest Hackathon",
+    description:
+      "Join HackMol 6.0, Punjab's leading hackathon, to tackle AI, Web3, and IoT challenges. Win ₹2L+ in prizes and connect with top mentors!",
+    images: ["https://hackmol.com/banner.png"],
+  },
+  robots: "index, follow",
+};
 
 export default function RootLayout({
   children,
@@ -23,6 +52,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" type="image/png" href="/favicon.png" />
+      </head>
       <body className={berserker.variable}>
         {children}
         <script defer async src="https://apply.devfolio.co/v2/sdk.js"></script>
